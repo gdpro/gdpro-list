ad8cc29e68122282fcb74d5cb84796fc353b4719
gary <gary.gitton@gmail.com>
2014-09-30 01:46:36 +0200

    up

--- Module.php
+++ Module.php
@@ -22,11 +22,11 @@ class Module
     public function getAutoloaderConfig()
     {
         return array(
-            'Zend\Loader\StandardAutoloader' => array(
-                'namespaces' => array(
-                    __NAMESPACE__ => __DIR__ . '/src/',
-                ),
-            ),
+            'Zend\Loader\StandardAutoloader' => [
+                'namespaces' => [
+                    __NAMESPACE__ => __DIR__ . '/src/'
+                ]
+            ]
         );
     }
 }
--- config/module.config.php
+++ config/module.config.php
@@ -1,22 +1,16 @@
 <?php
 namespace GdproList;
+
 return [
     'service_manager' => [
         'factories' => [
-            'gdpro.list.account_type' =>
-                'GdproList\Factory\AccountTypeListFactory',
-            'gdpro.list.advertisement_status' =>
-                'GdproList\Factory\AdvertisementStatusListFactory',
-            'gdpro.list.gender' =>
-                'GdproList\Factory\GenderListFactory',
-            'gdpro.list.honorific' =>
-                'GdproList\Factory\HonorificListFactory',
-            'gdpro.list.quotation_status' =>
-                'GdproList\Factory\QuotationStatusListFactory',
-            'gdpro.list.taxable_horse_power' =>
-                'GdproList\Factory\TaxableHorsePowerListFactory',
-            'gdpro.list.vehicle_state' =>
-                'GdproList\Factory\VehicleStateListFactory',
+            'gdpro.list.account_type' => 'GdproList\Factory\AccountTypeListFactory',
+            'gdpro.list.advertisement_status' => 'GdproList\Factory\AdvertisementStatusListFactory',
+            'gdpro.list.gender' => 'GdproList\Factory\GenderListFactory',
+            'gdpro.list.honorific' => 'GdproList\Factory\HonorificListFactory',
+            'gdpro.list.quotation_status' => 'GdproList\Factory\QuotationStatusListFactory',
+            'gdpro.list.taxable_horse_power' => 'GdproList\Factory\TaxableHorsePowerListFactory',
+            'gdpro.list.vehicle_state' => 'GdproList\Factory\VehicleStateListFactory',
         ]
     ],
 
