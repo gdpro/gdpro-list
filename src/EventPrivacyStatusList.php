a93f6b5df5247c491229eb21ac80df6566d687e0
gary <gary.gitton@gmail.com>
2014-10-06 20:45:29 +0200

    add Event Privay and Gender list

---
+++ src/EventPrivacyStatusList.php
@@ -0,0 +1,32 @@
+<?php
+namespace GdproList;
+
+class EventPrivacyStatusList extends AbstractList
+{
+    const IS_PUBLIC = 0;
+    const IS_PROTECTED = 1;
+    const HOST_INVITATION = 2;
+    const OPEN_INVITATION = 3;
+
+    /**
+     * List of event pr
+     * @var array
+     */
+    private $eventPrivacyStatusList = [
+        self::IS_PUBLIC => 'Public',
+        self::IS_PROTECTED => 'Protected',
+        self::HOST_INVITATION => 'Host invitation',
+        self::OPEN_INVITATION => 'Open invitation',
+    ];
+
+
+
+    public function getAllEventPrivacyStatuses($langage = 'en')
+    {
+        if($langage == 'fr') {
+            return $this->eventPrivacyStatusListFr;
+        }
+
+        return $this->eventPrivacyStatusList;
+    }
+}
--- src/GenderList.php
+++ src/GenderList.php
@@ -3,11 +3,11 @@ namespace GdproList;
 
 class GenderList extends AbstractList
 {
-    const MAN = 1;
-    const WOMAN = 2;
+    const MALE = 1;
+    const FEMALE = 2;
 
     protected $list = [
-        self::WOMAN => 'Woman',
-        self::MAN => 'Man'
+        self::MALE => 'Male',
+        self::FEMALE => 'Female'
     ];
 }
