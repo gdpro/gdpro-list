187e680a401c38dcaace8633eb51f4f3b40c02b2
gary <gary.gitton@gmail.com>
2014-11-06 00:36:30 +0100

    add roles manager and customer

--- src/UserRoleList.php
+++ src/UserRoleList.php
@@ -11,11 +11,15 @@ class UserRoleList extends AbstractList
     const ADMINISTRATOR = 2;
     const PROFESSIONAL = 3;
     const INDIVIDUAL = 4;
+    const CUSTOMER = 5;
+    const MANAGER = 6;
 
     protected $list = [
         self::GUEST => 'Guest',
         self::ADMINISTRATOR => 'Administrator',
         self::PROFESSIONAL => 'Professional',
-        self::INDIVIDUAL => 'Individual'
+        self::INDIVIDUAL => 'Individual',
+        self::CUSTOMER => 'Customer',
+        self::MANAGER => 'Manager'
     ];
 }
