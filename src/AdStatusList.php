d7bd6dd9dc2a4f67cb9f387e11a834624f50d265
gary <gary.gitton@gmail.com>
2014-11-10 18:38:31 +0100

    add value closed to advertisement statuses

--- src/AdStatusList.php
+++ src/AdStatusList.php
@@ -7,11 +7,13 @@ class AdStatusList extends AbstractList
     const WAITING_FOR_MODERATION = 2;
     const PUBLISHED = 3;
     const WAITING_FOR_MODIFICATION = 4;
+    const CLOSED = 5;
 
     protected $list = [
         self::CREATED => 'Created',
         self::WAITING_FOR_MODERATION => 'Waiting for moderation',
         self::PUBLISHED => 'Published',
         self::WAITING_FOR_MODIFICATION => 'Waiting for modification',
+        self::CLOSED => 'Closed'
     ];
 }
--- src/AdvertisementStatusList.php
+++ src/AdvertisementStatusList.php
@@ -9,13 +9,11 @@ class AdvertisementStatusList extends AbstractList
     const WAITING_FOR_MODIFICATION = 4;
     const CLOSED = 5;
 
-
     protected $list = [
         self::CREATED => 'Created',
         self::WAITING_FOR_MODERATION => 'Waiting for moderation',
         self::PUBLISHED => 'Published',
         self::WAITING_FOR_MODIFICATION => 'Waiting for modification',
         self::CLOSED => 'Closed',
-
     ];
 }
