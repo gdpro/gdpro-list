a471d50018880f1a911da75e92789b701b7ea241
gary <gary.gitton@hotmail.fr>
2014-12-03 05:57:52 +0100

    add default translator

--- Module.php
+++ Module.php
@@ -1,17 +1,35 @@
 <?php
 namespace GdproList;
 
+use Zend\Mvc\MvcEvent;
+use Zend\Validator\AbstractValidator;
+
 /**
  * Class Module
  * @package GdproList
  */
 class Module
 {
+    /**
+     * @param MvcEvent $e
+     */
+    public function onBootstrap(MvcEvent $e)
+    {
+        $translator = $e->getApplication()->getServiceManager()->get('translator');
+        AbstractValidator::setDefaultTranslator($translator);
+    }
+
+    /**
+     * @return mixed
+     */
     public function getConfig()
     {
         return include __DIR__ . '/config/module.config.php';
     }
 
+    /**
+     * @return array
+     */
     public function getAutoloaderConfig()
     {
         return array(
