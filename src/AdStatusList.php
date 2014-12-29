d0973c8f87ed5b0dc3c0f9b3d9a6500b254472eb
gary <gary.gitton@hotmail.fr>
2014-12-30 00:54:12 +0100

    add todo

--- src/AdStatusList.php
+++ src/AdStatusList.php
@@ -5,6 +5,7 @@ class AdStatusList extends AbstractList
 {
     const CREATED = 1;
     const WAITING_FOR_MODERATION = 2;
+    /** TODO: modify published by moderated */
     const PUBLISHED = 3;
     const WAITING_FOR_MODIFICATION = 4;
     const CLOSED = 5;
