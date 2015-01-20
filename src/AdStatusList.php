b00533a2e7134a250b5cdf3e583f97adf141d72d
gary <gary.gitton@hotmail.fr>
2015-01-20 03:12:35 +0100

    add new status and deprecated some

--- src/AdStatusList.php
+++ src/AdStatusList.php
@@ -3,18 +3,50 @@ namespace GdproList;
 
 class AdStatusList extends AbstractList
 {
+    /**
+     * The individual created the ad, but is still not visible
+     */
     const CREATED = 1;
-    const WAITING_FOR_MODERATION = 2;
-    /** TODO: modify published by moderated */
-    const PUBLISHED = 3;
-    const WAITING_FOR_MODIFICATION = 4;
+
+    /**
+     * The admin check the ad and validated it to be visible to others
+     * user
+     */
+    const VALIDATED = 6;
+
+    /**
+     * The admin check the ad and moderate it. The ad can't be visible to
+     * other user
+     */
+    const MODERATED = 3;
+
+    /**
+     * The individual choose a quotation, the ad is no longer open. We close it
+     */
     const CLOSED = 5;
+
+    /**
+     * @deprecated
+     */
+    const WAITING_FOR_MODERATION = 2;
+
+    /**
+     * @deprecated
+     */
+    const PUBLISHED = 3;
+
+    /**
+     * @deprecated
+     */
+    const WAITING_FOR_MODIFICATION = 4;
 
     protected $list = [
         self::CREATED => 'Created',
+        self::VALIDATED => 'Validated',
+        self::MODERATED => 'Moderated',
+        self::CLOSED => 'Closed',
         self::WAITING_FOR_MODERATION => 'Waiting for moderation',
         self::PUBLISHED => 'Published',
         self::WAITING_FOR_MODIFICATION => 'Waiting for modification',
-        self::CLOSED => 'Closed'
     ];
 }
--- src/QuotationStatusList.php
+++ src/QuotationStatusList.php
@@ -3,13 +3,48 @@ namespace GdproList;
 
 class QuotationStatusList extends AbstractList
 {
+    /**
+     * The professional created the quotation, but is still not visible for the
+     * individual
+     */
     const CREATED = 1;
-    const WAITING_FOR_MODERATION = 2;
+
+    /**
+     * The admin check the quotation and validated it to be visible to others
+     * user
+     */
+    const VALIDATED = 8;
+
+    /**
+     * The admin check the quotation and moderate it. The quotation can't be
+     * visible to other user
+     */
     const MODERATED = 3;
-    const WAITING_FOR_MODIFICATION = 4;
-    const SENT = 5;
+
+    /**
+     * The individual accept the quotation.
+     */
     const ACCEPTED = 6;
+
+    /**
+     * The individual reject the quotation.
+     */
     const REJECTED = 7;
+
+    /**
+     * @deprecated
+     */
+    const WAITING_FOR_MODERATION = 2;
+
+    /**
+     * @deprecated
+     */
+    const WAITING_FOR_MODIFICATION = 4;
+
+    /**
+     * @deprecated
+     */
+    const SENT = 5;
 
     protected $list = [
         self::CREATED => 'Created',
@@ -18,6 +53,7 @@ class QuotationStatusList extends AbstractList
         self::WAITING_FOR_MODIFICATION => 'Waiting for modification',
         self::SENT => 'Sent',
         self::ACCEPTED => 'Accepted',
-        self::REJECTED => 'Rejected'
+        self::REJECTED => 'Rejected',
+        self::VALIDATED => 'Validated'
     ];
 }
